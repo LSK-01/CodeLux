d11d364291ca3808e2e80a129dc738bae5dc3ba7
Naomi <naomiirider@gmail.com>
2023-02-11 18:16:22 +0000

    Fixed Likert scale centering

--- src/routes/forms/+page.svelte
+++ src/routes/forms/+page.svelte
@@ -73,4 +73,12 @@
         width: 100%;
         margin: 0 auto;
     }
+    .vl {
+        border-left: 6px solid green;
+        height: 500px;
+        position: absolute;
+        left: 50%;
+        margin-left: -3px;
+        top: 0;
+    }
 </style>
