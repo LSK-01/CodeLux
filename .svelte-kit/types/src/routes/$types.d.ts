66497affd197116b3e9bd3d00eae90b6187170ee
Naomi <naomiirider@gmail.com>
2023-02-11 19:53:52 +0000

    Add tailwind

--- .svelte-kit/types/src/routes/$types.d.ts
+++ .svelte-kit/types/src/routes/$types.d.ts
@@ -9,7 +9,7 @@ type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Pa
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
 type PageParentData = EnsureDefined<LayoutData>;
-type LayoutRouteId = RouteId | "/" | "/about" | "/forms" | "/sverdle" | "/sverdle/how-to-play" | null
+type LayoutRouteId = RouteId | "/" | "/about" | "/forms" | "/login" | "/sverdle" | "/sverdle/how-to-play" | null
 type LayoutParams = RouteParams & {  }
 type LayoutParentData = EnsureDefined<{}>;
 
