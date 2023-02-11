4235592844f7c0acbb252013ff4648f9070445bd
alvin-agidi <alvin.agidi@gmail.com>
2023-02-11 17:54:02 +0000

    Minimal Svelte App

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,9 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visi <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	
 </div>
 
 <style>
