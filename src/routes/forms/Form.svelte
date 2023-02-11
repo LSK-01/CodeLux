a06cd3845a32a9443b0ab5e807f2f9e8f21487ee
Naomi <naomiirider@gmail.com>
2023-02-11 17:45:00 +0000

    Initial commit with bad formatting

--- src/routes/forms/Form.svelte
+++ src/routes/forms/Form.svelte
@@ -1,38 +1,77 @@
+<!-- https://svelte.dev/repl/7b05d57dcdc04f49be72844e4b2825b3?version=3.44.0 -->
 <script lang="ts">
-    //https://firebase.google.com/docs/web/setup
-    // import { initializeApp } from 'firebase/app';
-    // import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-    // // Follow this pattern to import other Firebase services
-    // // import { } from 'firebase/<service>';
+    import Radio from './Radio.svelte';
+	
+	export let active_step : number;
+	export let options : any[number][string];    
+	export let questions : string[];
 
-    // // TODO: Replace the following with your app's Firebase project configuration
-    // const firebaseConfig = {
-    // //...
-    // };
+	let formData = Array(questions.length);
+	
+    //TODO: POST data back to database or however firebase works
+	const handleSubmit = () => {
+		console.log("Your form data => ",formData)
+	}
 
-    // const app = initializeApp(firebaseConfig);
-    // const db = getFirestore(app);
-
-    // // Get a list of cities from your database
-    // async function getCities(db) {
-    // const citiesCol = collection(db, 'cities');
-    // const citySnapshot = await getDocs(citiesCol);
-    // const cityList = citySnapshot.docs.map(doc => doc.data());
-    // return cityList;
-    // }
-    function getQuestions(){
-        //will be different based on employee or manager
-        const questions = [
-            "I have received adequate training to help me complete the project", 
-            "My manager supports me in any training I want to undertake to help me perform my role better", 
-            "My team is easy to communicate with", 
-            "My team work well together", 
-            "I feel recognised and valued for my role and contribution to this project", 
-            "I enjoy being a part of my company’s culture", 
-            "I feel confident the project will be finished on time",
-            "My team have the resources and skills necessary to complete the project", 
-            "I feel satisfied with the frequency of feedback received from the customer" 
-        ];
-        return questions;
-    }
 </script>
+
+<!-- TODO: redirect user to another page when complete -->
+<form class="form-container" on:submit={handleSubmit}>
+	{#if active_step != questions.length+1}
+        <div class="centered">
+            <h1 class="qheading">
+                Question {active_step}
+            </h1>
+            <div class="padding">
+                <Radio {options} question={questions[active_step-1]} bind:userSelected={formData[active_step-1]}/>
+            </div>
+        </div>        
+	{:else}
+		<div class="message">
+            <h1 class="qheading">
+                Confirmation
+            </h1>			
+			<h2>Thank you for completing the survey</h2>
+			<button class="btn submit">Submit</button>
+		</div>
+	{/if}
+</form>
+
+<style>
+	.qheading {
+        font-weight: bold;
+        font-size: 24px;
+    }
+    .padding {
+        padding-top: 10px;
+    }
+	.form-container {
+		background-color: #fff;
+		border-radius: 10px;
+		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
+		padding: 50px 20px;
+		text-align: center;
+		max-width: 100%;
+		width: 100%;
+	}
+	.btn{
+		color: white;
+		padding: 0.5rem 0;
+		margin-top: 0.5rem;
+		display: inline-block;
+		width: 100%;
+		border-radius: 0.25rem;
+		cursor:pointer;
+	}
+	.submit{
+		background:linear-gradient(to bottom, #44c767 5%, #50b01c 100%);
+		background-color:#44c767;
+	}
+	.submit:hover {
+		background:linear-gradient(to bottom, #50b01c 5%, #44c767 100%);
+		background-color:#50b01c;
+	}
+	.message{
+		text-align: center;
+	}
+</style>
--- src/routes/forms/Radio.svelte
+++ src/routes/forms/Radio.svelte
@@ -1,16 +1,14 @@
 <!-- https://codepen.io/mr-t77/pen/jRzmMZ -->
 
 <script lang="ts">
-  export let options;
-  export let question: string;
-  export let userSelected = options[0].value;
+  export let options : any[number][string];
+  export let question : string;
+  export let userSelected : number;
 </script>  
 
 <div class="centered">
   <form action="">
-    <div class="padding">
-      <label class="statement">{question}</label>
-    </div>
+    <div class="padding">{question}</div>
     <ul class='likert'>
       {#each options as { value, label }}
         <li>
@@ -24,14 +22,10 @@
 
 <style>
     .padding{
-      padding-bottom: 5px;
+      padding-bottom: 20px;
     }
     .centered{
         margin: 0 auto;
-    }
-    .statement{
-        text-align: center;
-        font-size: 16px;
     }
     .subh{
         font-size: 11px;
@@ -60,16 +54,5 @@
     top:0;
     left:50%;
     margin-left:-6px;
-    
     }
-    form .buttons button {
-    padding: 5px 10px;
-    background-color: #67ab49;
-    border: 0;
-    border-radius: 3px;
-    }
-    form .buttons .clear {background-color: #e9e9e9;}
-    form .buttons .submit {background-color: #67ab49;} 
-    form .buttons .clear:hover {background-color: #ccc;}
-    form .buttons .submit:hover {background-color: #14892c;} 
 </style>
