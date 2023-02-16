7d75eadce8c65b2374944cc53677268a5abb6acf
alvin-agidi <alvin.agidi@gmail.com>
2023-02-16 13:18:19 +0000

    Reorganising after merge

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import './styles.css';
 	import { page } from '$app/stores';
-	let navItems = ['about', 'login', 'signup'];
+	import Header from './Header.svelte';
+	let navItems = ['about', 'login', 'signup',  'dashboard'];
 </script>
 
 <div class="app">
@@ -16,48 +17,3 @@
 		<p>Pimp My Project</p>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-		justify-content: space-evenly;
-		flex: 1;
-		padding: 0 10vw;
-	}
-
-	#wrapper {
-		display: flex;
-		flex: 1;
-		margin: 10px 0;
-		flex-direction: column;
-		gap: 10px;
-	}
-
-	#dashboard {
-		flex: 1;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-evenly;
-		gap: 10px;
-		flex-wrap: wrap;
-	}
-
-	#lowerDashboard {
-		flex: 1;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-evenly;
-	}
-
-	.boxContents {
-		flex: 1 0;
-		display: flex;
-		flex-direction: column;
-		padding: 5px 0;
-		border-radius: 10px;
-		background-color: var(--fg1);
-	}
-
-</style>
