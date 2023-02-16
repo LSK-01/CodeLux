b622310f77b82cbfe2b9ba94b6ca7560da6160e5
alvin-agidi <alvin.agidi@gmail.com>
2023-02-16 16:41:26 +0000

    Added project counts to dashboard

--- src/routes/dashboard/projectStatusesBox.svelte
+++ src/routes/dashboard/projectStatusesBox.svelte
@@ -5,7 +5,18 @@
 <div id='projectStatusesBox'>
 	<h2>Projects</h2>
 	<div class='boxContents'>
-		<p></p>
+		<div class='overviewBox'>
+			<p>Projects at risk</p>
+			<h2>projectsAtRiskCount</h2>
+		</div>
+		<div class='overviewBox'>
+			<p>Projects with surveys due</p>
+			<h2>projectsAtSurveysCount</h2>
+		</div>
+		<div class='overviewBox'>
+			<p>Projects at tasks due</p>
+			<h2>projectsAtTaskCount</h2>
+		</div>
 	</div>
 </div>
 
@@ -28,5 +39,21 @@
 		border-radius: 5px;
 		background-color: var(--fg1);
 		box-shadow: inset 0 0 10px rgba(0, 0, 0);
+		gap: 10px;
+		padding: 10px;
+		justify-content: space-evenly;
 	}
+
+	.overviewBox {
+		flex: 1 0;
+		display: flex;
+		flex-direction: column;
+		padding: 5px 0;
+		border-radius: 5px;
+		background-color: var(--fg2);
+		border: var(--text);
+		align-items: center;
+		justify-content: center;
+	}
+
 </style>
