5e873e45dac2371eea3775ec5c5174af4ff82501
roysterC <125501340+roysterC@users.noreply.github.com>
2023-02-17 15:51:39 +0000

    added firebase

---
+++ .svelte-kit/generated/client-manifest.js
@@ -0,0 +1,21 @@
+export { matchers } from './client-matchers.js';
+
+export const nodes = [() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5')];
+
+export const server_loads = [];
+
+export const dictionary = {
+	"/": [2],
+	"/about": [3],
+	"/login": [~4],
+	"/signup": [~5]
+};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+};
