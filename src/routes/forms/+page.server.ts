5d851a7b3d2fdc279921cd0204e65946922b97b0
Naomi <naomiirider@gmail.com>
2023-02-18 21:19:48 +0000

    Remove comments

--- src/routes/forms/+page.server.ts
+++ src/routes/forms/+page.server.ts
@@ -24,17 +24,3 @@ export async function load() {
         });
     return { post: questions }
     }
-
-    //will be different based on employee or manager
-    // const questions = [
-    //     "I have received adequate training to help me complete the project", 
-    //     "My manager supports me in any training I want to undertake to help me perform my role better", 
-    //     "My team is easy to communicate with", 
-    //     "My team work well together", 
-    //     "I feel recognised and valued for my role and contribution to this project", 
-    //     "I enjoy being a part of my company’s culture", 
-    //     "I feel confident the project will be finished on time",
-    //     "My team have the resources and skills necessary to complete the project", 
-    //     "I feel satisfied with the frequency of feedback received from the customer" 
-    // ];
-    // return questions;
