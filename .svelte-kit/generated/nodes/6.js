8611c168128e261a08621c808b62c56cffc59eee
Naomi <naomiirider@gmail.com>
2023-02-18 21:09:45 +0000

    Remove .svelte-kit

--- .svelte-kit/generated/nodes/6.js
+++
@@ -1,6 +0,0 @@
-export { default as component } from "../../../src/routes/signup/+page.svelte";
-<<<<<<< HEAD
-export const has_server_load = true;
-=======
-export const has_server_load = true;
->>>>>>> 242a57605c4637dfa68f9846c0057dbc313182a1
