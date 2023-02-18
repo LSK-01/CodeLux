93270003c33fb5700a8c6fb7507b04cd82f592b5
Naomi <naomiirider@gmail.com>
2023-02-18 19:50:54 +0000

    Fix db

--- .svelte-kit/generated/nodes/4.js
+++ .svelte-kit/generated/nodes/4.js
@@ -1,3 +1,2 @@
-import * as universal from "../../../src/routes/forms/+page.ts";
-export { universal };
 export { default as component } from "../../../src/routes/forms/+page.svelte";
+export const has_server_load = true;
