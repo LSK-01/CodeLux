f59be6842f574f04a20900c840344665dfee4d7b
alvin-agidi <alvin.agidi@gmail.com>
2023-02-22 20:07:10 +0000

    Initial setup brav

---
+++ src/routes/project_overview/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import '../styles.css';
+  // import Sidebar from "./sidebar.svelte";
+</script>
+
+<svelte:head>
+  <title>Overview</title>
+</svelte:head>
+
+<div class="app">
+  <div id="wrapper">
+    <!-- <Sidebar /> -->
+    <div id="overview">
+    </div>
+  </div>
+</div>
+
+<style>
+  .app {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    justify-content: space-evenly;
+    flex: 1;
+    padding: 0 10vw;
+  }
+
+  #wrapper {
+    display: flex;
+    flex: 1;
+    margin: 10px 0;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  #overview {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    gap: 10px;
+    flex-wrap: wrap;
+  }
+</style>
