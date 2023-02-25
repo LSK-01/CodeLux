ff63b0a9d23f619ccf7e8c22ed8adf3074050e6f
preyat <preya.thaker@gmail.com>
2023-02-25 21:33:27 +0000

    tidying in proccess

--- src/routes/add_project/components/FeedbackForm.svelte
+++ src/routes/add_project/components/FeedbackForm.svelte
@@ -2,50 +2,30 @@
   import {v4 as uuidv4} from 'uuid'
   import {FeedbackStore} from '../stores'
   import Card from './Card.svelte'
-  import Button from './Button.svelte'
   import RatingSelect from './RatingSelect.svelte'
 
   let text = ''
-  let rating = 10
-  let btnDisabled = true
-  let min = 10
-  let message
-  let questions =  ["Project Name","Project Description", "Manager Username","Developer Username(s)", "Project Time Frame", 
-                    "Github link to Source Code Files", "Satsfaction", "Confidence", "Customer Contact Frequency", "Customer Satsfaction",
-                    "Completion Percentage", "Budget", "Start Date", "Deadline"]
-  let questions_new = [["Project Name",false],["Project Description",true], ["Manager Username",true],
-                      ["Developer Username(s)",true], ["Project Time Frame",true], ["Github link to Source Code Files",true], 
-                      ["Satsfaction",false], ["Confidence",false], ["Customer Contact Frequency", true], ["Customer Satsfaction", true],
-                    ["Completion Percentage",true], ["Budget",true], ["Start Date",true],[ "Deadline",true]]                  
-  let count = 0
-
+  let rating = 0
+  let question_num = 0
+  let questions_new = [["Project Name",true],["Project Description",true], ["Manager Username",true],
+                      ["Developer Username(s)",true], ["Project Time Frame (in hours)",true], ["Github link to Source Code Files",true], 
+                      ["Satsfaction",false], ["Confidence",false], ["Customer Contact Frequency (times per month)", true], ["Customer Satsfaction", true],
+                    ["Completion Percentage",true], ["Budget (in british pounds)",true], ["Start Date (in format Day/Month/Year)",true],[ "Deadline  (in format Day/Month/Year)",true]]     
   handleReset()
-  const handleSelect = (e) => {
-    rating = e.detail
-    message = null
-    btnDisabled = false
+
+  const handleSelect = (numb) => {
+    rating = numb.detail
     text = ""
   }
 
   const handleInput = () => {
-    if(text.trim().length <= min) {
-      message = `Text must be at least ${min} characters`
-      btnDisabled = true
-    } else {
-      message = null
-      btnDisabled = false
-      rating = -1
-    }
+    rating = -1
   }
 
   const handleSubmit = () => {
-    if(text.trim().length > -1) {
-      if(text != ''){
-        rating = -1
-      }
       const newFeedback = {
         id: uuidv4(),
-        question : questions_new[count][0],
+        question : questions_new[question_num][0],
         text,
         rating: +rating,
       }
@@ -55,67 +35,45 @@
       })
 
       text = ''
-    }
 
-    count += 1
-    if (count == questions.length){
+    question_num += 1
+    if (question_num == questions.length){
       alert("All questions answered !")
       handleReset()
-      
     }
   }
 
   function handleReset() {
-      count = 0
+    question_num = 0
       FeedbackStore.update(() => {
         return []
       })
-      
   }
   
 </script>
 
 <h1 on:load={handleReset}> 
- 
   Add Project
 </h1>
 <Card > 
   <header>
-    <h2>{questions_new[count][0]}</h2>
+    <h2>{questions_new[question_num][0]}</h2>
   </header>
 <form >
-  {#if questions_new[count][1] == true}
+  {#if questions_new[question_num][1] == true}
     <div class="input-group">
       <input type="text" on:input={handleInput} bind:value = {text} placeholder="Answer here ">
     </div>
   {:else}
     <RatingSelect on:rating-select={handleSelect} />
   {/if}
-  <!--Button disabled={btnDisabled} type="submit">Send</Button-->
-  <button on:click={handleSubmit}>Send</button>
-  {#if message}
-    <div class="message">
-      {message}
-    </div>
-  {/if}
+  <br/>
+  <button on:click={handleSubmit} class=" bg-red-200 w-32 h-10 rounded-lg hover:bg-red-400 drop-shadow-md"><slot>Send</slot></button>
+  <button on:click={handleReset}  class=" bg-red-200 w-32 h-10 rounded-lg hover:bg-red-400 drop-shadow-md"><slot>Restart</slot> </button> 
 </form>
 </Card>
 
-<!--<button on:click={() => ( FeedbackStore.update((currentFeedback) => { return []}) )}> Add New Project </button>-->
-<button on:click={handleReset}> Add another project </button>
-
-
 <style>
-  header {
-    max-width: 400px;
-    margin: auto;
-  }
-
-  header h2 {
-    font-size: 22px;
-    font-weight: 600;
-    text-align: center;
-  }
 
   .input-group {
     display: flex;
@@ -126,19 +84,9 @@
     margin-top: 15px;
   }
 
-  input {
-    flex-grow: 2;
-    border: none;
-    font-size: 16px;
-  }
 
   input:focus {
     outline: none;
   }
 
-  .message{
-    padding-top: 10px;
-    text-align: center;
-    color: rebeccapurple;
-  }
 </style>
