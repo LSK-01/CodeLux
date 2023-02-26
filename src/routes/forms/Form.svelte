af7cae844b27a3aed56571fe806ad111586c5372
roysterC <125501340+roysterC@users.noreply.github.com>
2023-02-26 14:36:42 +0000

    simple version of survey, database working good
    chose to use a one page survey instead, database connected fully getting question no, question ans, uid, email and timestamp

--- src/routes/forms/Form.svelte
+++ src/routes/forms/Form.svelte
@@ -25,10 +25,6 @@
             </h1>
             <div class="padding">
                 <Radio {options} question={questions[active_step-1]} bind:userSelected={formData[active_step-1]}/>
-            </div>
-        </div>        
-	{:else}
-		<div class="message">
             <h1 class="qheading">
                 Confirmation
             </h1>			
@@ -36,6 +32,60 @@
 			<button class="btn submit">Submit</button>
 		</div>
 	{/if}
+</form>-->
+
+<form class="form-container"  on:submit={handleSubmit} method="POST" action="?/store">
+	<fieldset id="group1">
+		<div class="centered">
+            <h1 class="qheading">
+                Question 1
+            </h1>
+			My manager supports me in any training I want to undertake to help me perform my role better<br>
+			<input type="radio" name="q1" value=0>Strongly disagree
+			<input type="radio" name="q1"value=1>Disagree
+			<input type="radio" name="q1"value=2>Somewhat disagree
+			<input type="radio" name="q1"value=3>Neither agree nor disagree
+			<input type="radio" name="q1"value=4>Somewhat agree
+			<input type="radio" name="q1"value=5>agree   
+			<input type="radio" name="q1"value=6>Strongly agree
+		</div> 
+	  </fieldset>
+
+	<div class="padding"></div>
+	<fieldset id="group2">
+		<div class="centered">
+            <h1 class="qheading">
+                Question 2
+            </h1>
+			I enjoy being a part of my company’s culture<br>
+			<input type="radio" name="q2" value=0>Strongly disagree
+			<input type="radio" name="q2"value=1>Disagree
+			<input type="radio" name="q2"value=2>Somewhat disagree
+			<input type="radio" name="q2"value=3>Neither agree nor disagree
+			<input type="radio" name="q2"value=4>Somewhat agree
+			<input type="radio" name="q2"value=5>agree   
+			<input type="radio" name="q2"value=6>Strongly agree
+		</div> 
+	  </fieldset>
+
+	  <div class="padding"></div>
+	  <fieldset id="group3">
+		  <div class="centered">
+			  <h1 class="qheading">
+				  Question 3
+			  </h1>
+			  My team work well together<br>
+			  <input type="radio" name="q3" value=0>Strongly disagree
+			  <input type="radio" name="q3"value=1>Disagree
+			  <input type="radio" name="q3"value=2>Somewhat disagree
+			  <input type="radio" name="q3"value=3>Neither agree nor disagree
+			  <input type="radio" name="q3"value=4>Somewhat agree
+			  <input type="radio" name="q3"value=5>agree   
+			  <input type="radio" name="q3"value=6>Strongly agree
+		  </div> 
+		</fieldset>
+
+	  <button  type="submit" class="btn submit">Submit</button>
 </form>
 
 <style>
