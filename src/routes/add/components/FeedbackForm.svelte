d5e5361915e3e84d1693d0d1d678cc9d2a645b55
Luca SK <sarifluca@gmail.com>
2023-02-28 12:10:29 +0000

    updates

--- src/routes/add/components/FeedbackForm.svelte
+++ src/routes/add/components/FeedbackForm.svelte
@@ -42,7 +42,7 @@
 
     if (question_num == questions_new.length) {
       const response = await fetch("/addProj", {
-        method: "POST",
+        method: "GET",
         body: JSON.stringify(answers),
         headers: {
           "content-type": "application/json",
