151ae601bbb2621334966a91a736875ce90b1306
alvin-agidi <alvin.agidi@gmail.com>
2023-02-28 21:53:49 +0000

    Icons

--- src/routes/sidebar/sidebar.svelte
+++ src/routes/sidebar/sidebar.svelte
@@ -6,21 +6,21 @@
 </script>
 
 <svelte:head>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </svelte:head>
 
 <nav>
 	<h1>Pimp My Project</h1>
 	<Button>
 		<a href="/">
-			<span class="material-icons" id='userIcon'>account_circle</span>
+			<span class="material-symbols-outlined" id='userIcon'>account_circle</span>
 			<h2>{user.username}</h2>
 		</a>
 	</Button>
-	<Button><a href="/add"><span class="material-icons">add</span>Add project</a></Button>
-	<Button><a href="/settings"><span class="material-icons">settings</span>Settings</a></Button>
-	<Button><a href="/about"><span class="material-icons">info</span>About</a></Button>
-	<Button><a href="/login"><span class="material-icons">logout</span>Log out</a></Button>
+	<Button><a href="/add"><span class="material-symbols-outlined">add_circle</span>Add project</a></Button>
+	<Button><a href="/settings"><span class="material-symbols-outlined">settings</span>Settings</a></Button>
+	<Button><a href="/about"><span class="material-symbols-outlined">info</span>About</a></Button>
+	<Button><a href="/login"><span class="material-symbols-outlined">logout</span>Log out</a></Button>
 </nav>
 
 <style>
