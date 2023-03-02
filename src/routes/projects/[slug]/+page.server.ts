620f618b11b1ce136f2666e117540d88613f3c39
Naomi <naomiirider@gmail.com>
2023-03-02 15:34:37 +0000

    Add surveysdue function

--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 import { app } from '../../../hooks.server';
-import { getFirestore,collection, getDocs, query, orderBy, where } from 'firebase/firestore';
+import { getFirestore,collection, getDocs, query, orderBy, where, Timestamp } from 'firebase/firestore';
 import type { PageServerLoad } from "../../login/$types";
 import type { user } from "../../../user";
 
@@ -38,6 +38,19 @@ export const load: PageServerLoad = async ({cookies, params}) => {
 
             return { post : getAtRiskProjects(user)};
         }
+    }
+    if (params.slug === 'projectswithsurveysdue'){
+        if (cookie == null) {
+            return {
+                post : ["Projects with surveys due",[]],
+            }
+        }
+        else{
+            const user = JSON.parse(cookie);
+
+            return { post : getProjectsWithSurveysDue(user)};
+        }
+    }
 }
 
 async function getAllProjects(user: user){
@@ -119,4 +132,72 @@ async function getAtRiskProjects(user: user){
     returnArray.push(projects);
     return returnArray ;
 }
+
+async function getProjectsWithSurveysDue(user: user){
+    let returnArray : any[] = ["Projects with surveys due"];  
+    let projects : any[] = [];
+
+    const db = getFirestore(app);
+    const ps = collection(db, "projects");
+    const q6 = query(
+      ps,
+      where("managerusername", "==", user.username),
+      where("complete", "==", false)
+    );
+    const q7 = query(
+      ps,
+      where("developerusernames", "array-contains", user.username),
+      where("complete", "==", false)
+    );
+    const querySnapshot6 = await getDocs(q6);
+    const querySnapshot7 = await getDocs(q7);
+  
+    const surveyAnswers = collection(db,"surveyanswers");
+  
+    const currentTime = Timestamp.now();
+    const weekOldTimestamp = Timestamp.fromMillis(currentTime.toMillis() - 604800000);
+  
+    querySnapshot6.forEach(async (project) => {
+      const q8 = query(
+        surveyAnswers,
+        where("userid","==", user.uid),
+        where("projectid","==", project.id),
+        where("time",">", weekOldTimestamp),
+      ); //if this is not empty a survey has been taken in the last seven days so DON'T generate survey for it
+      const querySnapshot8 = await getDocs(q8);
+  
+      if (querySnapshot8.empty) {
+        projects.push({
+            projectName: project.data().projectname, 
+            dueDate: project.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: true})
+        };
+    });
+
+    querySnapshot7.forEach(async (project) => {
+        const q9 = query(
+          surveyAnswers,
+          where("userid","==", user.uid),
+          where("projectid","==", project.id),
+          where("time",">", weekOldTimestamp),
+        ); //if this is not empty a survey has been taken in the last seven days so DON'T generate survey for it
+        const querySnapshot9 = await getDocs(q9);
+    
+        if (querySnapshot9.empty) {
+          projects.push({
+              projectName: project.data().projectname, 
+              dueDate: project.data().deadline.toDate().toLocaleString("en-GB",{
+              year: "numeric",
+              month: "numeric",
+              day: "numeric",
+              }), managerBool: true})
+          };
+      });
+    returnArray.push(projects);
+    console.log("HELLO????")
+    console.log(returnArray);
+    return returnArray;
 }
