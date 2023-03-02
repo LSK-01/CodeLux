d1b400e1dd51ce37fd39bac655b8be7867a2d012
Naomi <naomiirider@gmail.com>
2023-03-02 15:04:56 +0000

    Add function

--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -2,6 +2,7 @@ import { dev } from '$app/environment';
 import { app } from '../../../hooks.server';
 import { getFirestore,collection, getDocs, query, orderBy, where } from 'firebase/firestore';
 import type { PageServerLoad } from "../../login/$types";
+import type { user } from "../../../user";
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
@@ -12,94 +13,110 @@ export const csr = dev;
 export const prerender = true;
 
 export const load: PageServerLoad = async ({cookies, params}) => {
+    const cookie = cookies.get('user')!;
+
     if (params.slug === 'all'){
-        let returnArray : any[] = ["All projects"];
-        let projects : any[] = [];
-        const cookie = cookies.get('user')!;
         if (cookie == null) {
             return {
-                post : projects,
+                post : ["All projects",[]],
             }
         }
-        const user = JSON.parse(cookie);
-        const db = getFirestore(app);
-        const projectsRef = collection(db, 'projects');
-        const q1 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",false), orderBy("deadline","asc"));
-        const q2 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",false), orderBy("deadline","asc"));
-        const q3 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",true), orderBy("deadline","asc"));
-        const q4 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",true), orderBy("deadline","asc"));
-        const querySnapshot1 = await getDocs(q1);
-        querySnapshot1.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: true})
-        });
-        const querySnapshot2 = await getDocs(q2);
-        querySnapshot2.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: false})
-        });
-        const querySnapshot3 = await getDocs(q3);
-        querySnapshot3.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: true})
-        });
-        const querySnapshot4 = await getDocs(q4);
-        querySnapshot4.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: false})
-        });
-        returnArray.push(projects);
-        return { post: returnArray };
+        else{
+            const user = JSON.parse(cookie);
+
+            return { post : getAllProjects(user)};
+        }
     }
     if (params.slug === 'atrisk'){
-        let returnArray : any[] = ["Projects at risk"];
-        let projects : any[] = [];
-        const cookie = cookies.get('user')!;
         if (cookie == null) {
             return {
-                post : projects,
+                post : ["Projects at risk",[]],
             }
         }
-        const user = JSON.parse(cookie);
-        const db = getFirestore(app);
-        const projectsRef = collection(db, 'projects');
-        const q1 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",false), where("atRisk","==",true), orderBy("deadline","asc"));
-        const q2 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",false), where("atRisk","==",true), orderBy("deadline","asc"));
-        const querySnapshot1 = await getDocs(q1);
-        querySnapshot1.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: true})
-        });
-        const querySnapshot2 = await getDocs(q2);
-        querySnapshot2.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            projects.push({projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
-                year: "numeric",
-                month: "numeric",
-                day: "numeric",
-                }), managerBool: false})
-        });
-        returnArray.push(projects);
-        return { post: returnArray };
-    }
+        else{
+            const user = JSON.parse(cookie);
+
+            return { post : getAtRiskProjects(user)};
+        }
+}
+
+async function getAllProjects(user: user){
+    let returnArray : any[] = ["All projects"];
+    let projects : any[] = [];
+    
+    const db = getFirestore(app);
+    const projectsRef = collection(db, 'projects');
+    const q1 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",false), orderBy("deadline","asc"));
+    const q2 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",false), orderBy("deadline","asc"));
+    const q3 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",true), orderBy("deadline","asc"));
+    const q4 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",true), orderBy("deadline","asc"));
+    const querySnapshot1 = await getDocs(q1);
+    querySnapshot1.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: true})
+    });
+    const querySnapshot2 = await getDocs(q2);
+    querySnapshot2.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: false})
+    });
+    const querySnapshot3 = await getDocs(q3);
+    querySnapshot3.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: true})
+    });
+    const querySnapshot4 = await getDocs(q4);
+    querySnapshot4.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({id: doc.id, projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: false})
+    });
+    returnArray.push(projects);
+    return returnArray;
+}
+
+async function getAtRiskProjects(user: user){
+    let returnArray : any[] = ["Projects at risk"];
+    let projects : any[] = [];
+
+    const db = getFirestore(app);
+    const projectsRef = collection(db, 'projects');
+    const q1 = query(projectsRef, where("managerusername", "==", user.username), where("complete","==",false), where("atRisk","==",true), orderBy("deadline","asc"));
+    const q2 = query(projectsRef, where("developerusernames", "array-contains", user.username), where("complete","==",false), where("atRisk","==",true), orderBy("deadline","asc"));
+    const querySnapshot1 = await getDocs(q1);
+    querySnapshot1.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: true})
+    });
+    const querySnapshot2 = await getDocs(q2);
+    querySnapshot2.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        projects.push({projectName: doc.data().projectname, dueDate: doc.data().deadline.toDate().toLocaleString("en-GB",{
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+            }), managerBool: false})
+    });
+    returnArray.push(projects);
+    return returnArray ;
+}
 }
