04dba5a223cf481ac8a16d4a509a07f681e80b85
Luca SK <sarifluca@gmail.com>
2023-03-06 18:35:50 +0000

    get num comits done

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,41 +1,52 @@
 <script lang="ts">
-  import '../styles.css';
+  import "../styles.css";
   import OverviewsBox from "./OverviewsBox.svelte";
   import TasksBox from "./TasksBox.svelte";
   import SurveysBox from "./SurveysBox.svelte";
   import DeadlinesBox from "./DeadlinesBox.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
+  import Button from "../Button.svelte";
+
+  const getNumCommits = async () => {
+	const response = await fetch('/getNumCommits', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+  };
 </script>
 
 <svelte:head>
-  	<title>Dashboard</title>
+  <title>Dashboard</title>
 </svelte:head>
 
 <div id="dashboard">
-	<OverviewsBox data={data} />
-	<div>
-	<DeadlinesBox deadlineList={data.deadlineList} />
-	<SurveysBox surveyList={data.surveyList} />
-	<TasksBox taskList={data.taskList} />
-	</div>
+  <OverviewsBox {data} />
+  <div>
+    <DeadlinesBox deadlineList={data.deadlineList} />
+    <SurveysBox surveyList={data.surveyList} />
+    <TasksBox taskList={data.taskList} />
+  </div>
+  <Button click={getNumCommits}>get num comimts</Button>
 </div>
 
 <style>
-	#dashboard {
-		display: flex;
-		min-height: 100vh;
-		justify-content: space-evenly;
-		margin: 10px 10vw;
-		gap: 10px;
-		flex-wrap: wrap;
-	}
+  #dashboard {
+    display: flex;
+    min-height: 100vh;
+    justify-content: space-evenly;
+    margin: 10px 10vw;
+    gap: 10px;
+    flex-wrap: wrap;
+  }
 
-	#dashboard div {
-		display: flex;
-		width: 100%;
-		justify-content: space-evenly;
-		gap: 10px;
-		height: 70vh;
-	}
+  #dashboard div {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    gap: 10px;
+    height: 70vh;
+  }
 </style>
