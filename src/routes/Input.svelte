36498a9345989c5d6ed0f1dd5b3c32c50076debb
alvin-agidi <alvin.agidi@gmail.com>
2023-03-11 22:46:54 +0000

    Login signup styling

--- src/routes/Input.svelte
+++ src/routes/Input.svelte
@@ -5,3 +5,14 @@
 </script>
 
 <input type={type} id="{name}" placeholder="{placeholder}" name="{name}" class=" bg-blue-200 drop-shadow-sm rounded-lg w-72 h-9 outline-slate-400 placeholder-slate-400 pl-3" />
+
+<style>
+    input{
+        display: inline-block;
+    }
+
+    input:focus {
+        outline: none;
+        background-color: var(--fg3);
+    }
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,13 +7,15 @@
   import { getIdToken } from "firebase/auth";
 
   export let form: ActionData;
+  let errorMsg = "";
 
   // if (form?.success == true) {
   //   if (browser) {
   //     goto("/dashboard");
   //   }
   // } else 
-  if (form?.success == false) {
+  if (form && form?.success == false) {
+    errorMsg = "Email or password was incorrect.";
     console.log("error when logging in");
   }
 </script>
@@ -22,20 +24,51 @@
   <title>Log in</title>
 </svelte:head>
 
-<div class="title">
-  <h1 class=" text-7xl">CodeLux</h1>
+
+<div id='logInBox'>
+  <div class="title">
+    <h1 class=" text-7xl">CodeLux</h1>
+  </div>
+  {#if errorMsg != ""}
+    <div id='errorBox'>
+      <span class="material-symbols-outlined">error</span>
+      <p>{errorMsg}<p>
+    </div>
+  {/if}
+  <form method="POST">
+    <section class="flex flex-col justify-center items-center flex-1 gap-5">
+      <Input type="email" name="email" placeholder="Email" />
+      <Input type="password" name="password" placeholder="Password" />
+      <Button>Log in</Button>
+    </section>
+  </form>
 </div>
 
-<form method="POST">
-  <section class="flex flex-col justify-center items-center flex-1 gap-5">
-    <Input type="email" name="email" placeholder="Email" />
-    <Input type="password" name="password" placeholder="Password" />
-    <Button>Log in</Button>
-  </section>
-</form>
 
 <style>
   .title {
     padding: 50px;
+  }
+
+  #logInBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    margin: auto;
+    border-radius: 10px;
+    background-color: var(--fg1);
+    box-shadow: var(--outset);
+    padding: 20px;
+    gap: 10px;
+  }
+
+  #errorBox {
+    display: flex;
+    background-color: var(--bad);
+    align-items: center;
+    border-radius: 10px;
+    padding: 10px;
+    gap: 5px;
   }
 </style>
