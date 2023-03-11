ed4f0daa8ec126f3e0361affee545a504f8e6a6c
alvin-agidi <alvin.agidi@gmail.com>
2023-03-11 22:46:54 +0000

    Login signup styling

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -6,36 +6,62 @@
   import { redirect } from "@sveltejs/kit";
   import { browser } from "$app/environment";
   import "../styles.css";
+  export let form: ActionData;
+  let errorMsg = "";
 
-  // export let form: ActionData;
-
-  // if (form?.success == true) {
-	// if(browser){
-	// 	goto("/dashboard");
-	// }
-  // }
+  if (form && !form?.success) {
+    errorMsg = "There was an error signing in.";
+  }
 </script>
-
 
 <svelte:head>
   <title>Sign up</title>
 </svelte:head>
 
-<div class="title">
-  <h1 class=" text-7xl">CodeLux</h1>
+<div id='signUpBox'>
+  <div class="title">
+    <h1 class=" text-7xl">CodeLux</h1>
+  </div>
+  {#if errorMsg != ""}
+    <div id='errorBox'>
+      <span class="material-symbols-outlined">error</span>
+      <p>{errorMsg}<p>
+    </div>
+  {/if}
+  <form method="POST">
+    <section class="flex flex-col justify-center items-center flex-1 gap-5">
+      <Input type="email" name="email" placeholder="Email" />
+      <Input type="password" name="password" placeholder="Password" />
+      <Button>Sign up</Button>
+    </section>
+  </form>
 </div>
-
-<form method="POST">
-  <section class="flex flex-col justify-center items-center flex-1 gap-5">
-    <Input type="email" name="email" placeholder="Email" />
-    <Input type="password" name="password" placeholder="Password" />
-    <Button on:click={()=>{goto("/dashboard")}}>Sign up</Button>
-  </section>
-</form>
 
 <style>
   .title{
     padding: 50px;
+  }
+
+  #signUpBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    margin: auto;
+    border-radius: 10px;
+    background-color: var(--fg3);
+    box-shadow: var(--outset);
+    padding: 20px;
+    gap: 10px;
+  }
+
+  #errorBox {
+    display: flex;
+    background-color: var(--bad);
+    align-items: center;
+    border-radius: 10px;
+    padding: 10px;
+    gap: 5px;
   }
 
   /* .entryWrapper{
