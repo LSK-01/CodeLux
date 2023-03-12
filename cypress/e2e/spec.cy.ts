9bbd17f7dbedd83e86a78fb9021bbb324e816153
Naomi <naomiirider@gmail.com>
2023-03-12 13:24:51 +0000

    Final tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,197 +1,193 @@
+
 const uuid = () => Cypress._.random(0, 1e6)
 const id = uuid()
 const testname = `cypress${id}`
 
-// describe('visits homepage', () => {
-//   it('visits homepage', () => {
-//     cy.visit('http://localhost:5173')
-//   })
-// })
+describe('visits homepage', () => {
+  it('visits homepage', () => {
+    cy.visit('http://localhost:5173')
+  })
+})
 
-// describe('finds header homepage buttons', () => {
-//   it('finds about button and redirects to about page', () => {
-//     cy.visit('http://localhost:5173')
+describe('finds header homepage buttons', () => {
+  it('finds log in button and redirects to log in page', () => {
+    cy.visit('http://localhost:5173')
 
-//     cy.contains('ABOUT').dblclick()
+    cy.contains('Log in').click()
 
-//     cy.url().should('eq', 'http://localhost:5173/about')
+  })
 
-//   })
+  it('finds signup button and redirects to signup page', () => {
+    cy.visit('http://localhost:5173')
 
-//   it('finds login button and redirects to login page', () => {
-//     cy.visit('http://localhost:5173')
+    cy.contains('Sign up').click()
 
-//     cy.contains('LOGIN').dblclick()
+  })
+})
 
-//     cy.url().should('eq', 'http://localhost:5173/login')
+describe('can sign up a user', () => {
 
-//   })
+  it('can sign up user and redirects to dashboard page', () => {
+      cy.visit('http://localhost:5173/signup')
 
-//   it('finds signup button and redirects to signup page', () => {
-//     cy.visit('http://localhost:5173')
-
-//     cy.contains('SIGNUP').dblclick()
-
-//     cy.url().should('eq', 'http://localhost:5173/signup')
-
-//   })
-// })
-
-// describe('finds homepage buttons', () => {
-
-//   it('finds login button and redirects to login page', () => {
-//     cy.visit('http://localhost:5173')
-
-//     cy.contains('Log in').dblclick()
-
-//   })
-
-//   it('finds signup button and redirects to signup page', () => {
-//     cy.visit('http://localhost:5173')
-
-//     cy.contains('Sign Up').dblclick()
-
-//   })
-// })
-
-// describe('can sign up a user', () => {
-
-  // it('finds signup button and redirects to signup page', () => {
-  //     cy.visit('http://localhost:5173')
-
-  //     cy.contains("Sign Up").dblclick()
+      const id = () => Cypress._.random(0, 1e6)
+      const signupname = `cypress${id()}@email.com`
   
-      // cy.url().should('eq', 'http://localhost:5173/signup')
+      cy.get("input[placeholder=Email]").type(signupname);
+  
+      cy.get("input[placeholder=Password]").click().type("password123").type('{enter}');
 
-      // const id = () => Cypress._.random(0, 1e6)
-      // const signupname = `cypress${id()}@email.com`
-  
-      // cy.get("input[placeholder=Email]").type(signupname);
-  
-      // cy.get("input[placeholder=Password]").type("password123");
-  
-      // cy.contains('Sign Up').dblclick()
-    
+      cy.url().should('eq', 'http://localhost:5173/dashboard')
   })
 
 
-// })
+})
 
-// describe('can log a user in', () => {
-//   it('finds log in button and redirects to login page', () => {
-//     cy.visit('http://localhost:5173')
+describe('can log a user in', () => {
+  it('finds log in button and redirects to login page', () => {
+    cy.visit('http://localhost:5173/login')
 
-//     cy.contains('LOGIN').dblclick()
+    cy.get("input[placeholder=Email]").type("cypress@email.com");
 
-//     cy.url().should('eq', 'http://localhost:5173/login')
+    cy.get("input[placeholder=Password]").click().type("password123").type('{enter}');
 
-//     cy.get("input[placeholder=Email]").type("cypress@email.com");
-
-//     cy.get("input[placeholder=Password]").type("password123");
-
-//     cy.contains('Login').dblclick()
-
-//     cy.login("cypress@email.com","password123")
+    cy.login("cypress@email.com","password123")
   
-//     cy.url().should('eq', 'http://localhost:5173/dashboard')
+    cy.url().should('eq', 'http://localhost:5173/dashboard')
 
-//     })    
-//   })
+    })    
+  })
 
-//   describe('can view all project pages', () => {
-//     beforeEach(() => {
-//       cy.visit('http://localhost:5173/login')
+describe('can log a user out', () => {
+  it('logs in a user, then can log it out', () => {
+    cy.visit('http://localhost:5173/login')
 
-//       cy.get("input[placeholder=Email]").type("cypress@email.com");
+    cy.get("input[placeholder=Email]").type("cypress@email.com");
 
-//       cy.get("input[placeholder=Password]").type("password123");
+    cy.get("input[placeholder=Password]").click().type("password123").type('{enter}');
 
-//       cy.contains('Login').dblclick()
+    cy.login("cypress@email.com","password123")
+  
+    cy.url().should('eq', 'http://localhost:5173/dashboard')
 
-//       cy.login("cypress@email.com","password123")
+    cy.contains('Log out').click()
 
-//     })
+    cy.url().should('eq', 'http://localhost:5173/login')
 
-//     it('can view all projects', () => {  
-//       cy.contains('View projects').click()
+    })    
+  })
 
-//       cy.url().should('eq', 'http://localhost:5173/projects/all')
-//       })   
+  describe('can view all project pages', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:5173/login')
+
+      cy.get("input[placeholder=Email]").type("cypress@email.com");
+
+      cy.get("input[placeholder=Password]").click().type("password123").type('{enter}');
+
+      cy.login("cypress@email.com","password123")
+
+    })
+
+    it('can view all projects', () => {  
+      cy.contains('View all projects').click()
+
+      cy.url().should('eq', 'http://localhost:5173/projects/all')
+      })   
       
-//     it('can view projects at risk', () => {
-//       cy.contains('Projects at risk').click()
+    it('can view projects at risk', () => {
+      cy.contains('Projects at risk').click()
 
-//       cy.url().should('eq', 'http://localhost:5173/projects/atrisk')
-//     })
+      cy.url().should('eq', 'http://localhost:5173/projects/atrisk')
+    })
 
-//     it('can view projects with surveys due', () => {
-//       cy.contains('Projects with surveys due').click()
+    it('can view projects with surveys due', () => {
+      cy.contains('Projects with surveys due').click()
 
-//       cy.url().should('eq', 'http://localhost:5173/projects/surveysdue')
-//     })
+      cy.url().should('eq', 'http://localhost:5173/projects/surveysdue')
+    })
 
-//     // it('can view projects with tasks due', () => {
-//     //   cy.contains('Projects with tasks due').click()
+    it('can view projects with tasks due', () => {
+      cy.contains('Projects with tasks due').click()
 
-//     //   cy.url().should('eq', 'http://localhost:5173/projects/tasksdue')
-//     // })
-//     })
+      cy.url().should('eq', 'http://localhost:5173/projects/tasksdue')
+    })
+    })
 
-  // describe('can add a project', () => {
-  //   it('adds a project', () => {
-  //     cy.visit('http://localhost:5173/login')
+  describe('can use a project', () => {
+    it('adds a project, takes a survey for it and uses project buttons', () => {
+      cy.visit('http://localhost:5173/login')
 
-  //     cy.get("input[placeholder=Email]").type("cypress@email.com")
+      cy.get("input[placeholder=Email]").type("cypress@email.com")
 
-  //     cy.get("input[placeholder=Password]").type("password123")
+      cy.get("input[placeholder=Password]").click().type("password123").type('{enter}')
 
-  //     cy.contains('Log in').dblclick()
-
-  //     cy.login("cypress@email.com","password123")
+      cy.login("cypress@email.com","password123")
       
-  //     cy.contains('Add project').dblclick()
+      cy.contains('Add project').dblclick()
 
-  //     cy.url().should('eq', 'http://localhost:5173/add')
+      cy.url().should('eq', 'http://localhost:5173/add')
 
 
 
-  //     cy.get("input[placeholder=\"Answer here\"]").type(testname)
+      cy.get("input[placeholder=\"Answer here\"]").type(testname)
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input[placeholder=\"Answer here\"]").type("description")
+      cy.get("input[placeholder=\"Answer here\"]").type("description")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("select").select("Go")
+      cy.get("select").select("Go")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input[placeholder=\"Answer here\"]").type("cypress")
+      cy.get("input[placeholder=\"Answer here\"]").type("cypress")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input[placeholder=\"Answer here\"]").type("cypressdev")
+      cy.get("input[placeholder=\"Answer here\"]").type("cypressdev")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input[placeholder=\"Answer here\"]").type("https://github.com/LSK-01/Hetris")
+      cy.get("input[placeholder=\"Answer here\"]").type("https://github.com/LSK-01/Hetris")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input").type("0.01")
+      cy.get("input").type("0.01")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input").type("2023-10-10")
+      cy.get("input").type("2023-10-10")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.get("input").type("2023-11-10")
+      cy.get("input").type("2023-11-10")
 
-  //     cy.contains('Send').click()
+      cy.contains('Send').click()
 
-  //     cy.contains(testname).should('exist');
-  //   })
+      cy.contains(testname).should('exist');
 
-  // })
+      var pid = "";
+      cy.url().then(url => {
+        pid = url.split('?')[1];
+
+        cy.visit(`http://localhost:5173/surveys?${pid}`)
+
+        cy.get('[type="radio"]').click({multiple : true})
+
+        cy.contains('Submit survey').click()
+
+        cy.contains("Go to dashboard").should('exist');
+
+        cy.contains("All projects").should('exist');
+
+        cy.visit(`http://localhost:5173/project_overview?${pid}`)
+
+        cy.contains("Mark as complete").click();
+
+      })
+  })
+
+
+  })
